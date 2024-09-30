ae84e1565f6ba0b45ba4109bd670736be80c921c
sashabunio <shabunio@inbox.ru>
2024-09-30 11:03:20 +0300

    Updated homework

--- Helloworld/src/Main.java
+++ Helloworld/src/Main.java
@@ -11,18 +11,24 @@ public class Main {
         String myName = "Aliaksanda";
         String mySurname = "Shabunio";
 
-        String infoAboutMe = String.format("My age: %s , My gender: %s , My name: %s , My surname: %s ",
-                myAge, myGender, myName, mySurname); // как обозначать дробное число?
+        String infoAboutMe = String.format("My age: %s , My gender: %s , My name: %s , My surname: %s , My Height : %f ",
+                myAge, myGender, myName, mySurname, myHeight);
         System.out.println(infoAboutMe);
-        System.out.println ("My Gender is " + myGender + " " + ", My name is " + myName + " "  + ", My Surname is "
-                + mySurname + " " + ", My Age is " + myAge +  " " + ", My Height is " + myHeight);
+        System.out.println("My Gender is " + myGender + " , My name is " + myName + " , My Surname is "
+                + mySurname + " , My Age is " + myAge + " , My Height is " + myHeight);
         StringBuffer buffer = new StringBuffer();
-        buffer.append("My Gender is " + myGender + " " + ", My name is " + myName + " "  + ", My Surname is "
-                + mySurname + " " + ", My Age is " + myAge +  " " + ", My Height is " + myHeight);
+        buffer.append("My Name is: ");
+        buffer.append(myName);
+        buffer.append("\nMy Surname is: ");
+        buffer.append(mySurname);
+        buffer.append("\nMy Age is: ");
+        buffer.append(myAge);
+        buffer.append("\nMy Height is: ");
+        buffer.append(myHeight);
+        buffer.append("\nMy Gender is: ");
+        buffer.append(myGender);
         System.out.println(buffer);
-        System.out.println ("My name is Aliaksandra: " + myNameIsAliaksandra);
-
-
+        System.out.println("My name is Aliaksandra: " + myNameIsAliaksandra);
 
         Scanner scanner = new Scanner(System.in);
         String myString = scanner.nextLine();
@@ -31,43 +37,22 @@ public class Main {
         int length = 10;
         boolean myStringLengthComparison = stringLength == length;
         System.out.println(myStringLengthComparison);
-
         boolean stringisEmpty = myString.isEmpty();
         System.out.println(stringisEmpty);
-
         char charNr8 = myString.charAt(8);
         System.out.println(charNr8);
-
         String stringforComparison = "my new string";
         boolean stringsareEqual = myString.equals(stringforComparison);
         boolean stringsareEqualCaseSensetive = myString.equalsIgnoreCase(stringforComparison);
         System.out.println(stringsareEqual);
         System.out.println(stringsareEqualCaseSensetive);
-
         boolean myStringStartsWith = myString.startsWith("My");
         System.out.println(myStringStartsWith);
         boolean myStringContains = myString.contains("test");
         System.out.println(myStringContains);
         boolean myStringEndsWith = myString.endsWith("ing");
         System.out.println(myStringEndsWith);
-
         String updatedString = myString.replace('s', 'S');
         System.out.println(updatedString);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
